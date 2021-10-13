a79f6c10bea673a0ff78c5fe715512586e11122d
Diego Mucheniski <mucheniski@gmail.com>
2021-10-13 14:33:56 -0300

    Initial commit

---
+++ src/test/java/com/example/jmsimplementation/JmsimplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmsimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsimplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
